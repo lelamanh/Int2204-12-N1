d1943f7f4c1057f04cd83bf55c0c2bb25a81615d
thjenhop <37095063+thjenhop@users.noreply.github.com>
2018-09-27 12:53:20 +0700

    Lab 03 - Ex03
    10 Object

---
+++ Group1/17021389/lab03/Lab03_03/People/Developer.java
@@ -0,0 +1,9 @@
+package Lab03_03.People;
+
+public class Developer extends EmployerCompany {
+    static String MAIN_WORK = "Programmers are computer programmers or other electronic means.";
+
+    public void setMainWork(String MAIN_WORK){
+        super.setMainWork(MAIN_WORK);
+    }
+}
