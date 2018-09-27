5c206c9620fa0db30a6af53dd75f3e37eec51803
maihanh <maihanhngo@gmail.com>
2018-09-28 00:24:55 +0700

    Lab03

---
+++ Group1/17021387/lab03/Lab03.3/Laptop.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class Laptop {
+
+    String name;
+    String color;
+    int price;
+
+    public Laptop(String name, String color, int price) {
+        this.name = name;
+        this.color = color;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
